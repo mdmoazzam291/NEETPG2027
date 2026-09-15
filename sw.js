a796f56cdf7d09f47aaebcd9d087bf798be47356
Md Moazzam <mdmoazzam291@gmail.com>
2026-09-15 07:03:02 +0530

    Cache NEET-PG timer enhancement for offline use

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='neetpg2027-v2-2026-09-15';
-const ASSETS=['./','./index.html','./assets/app.css','./assets/app.js','./manifest.webmanifest','./data/high_yield_100/part01.json','./data/high_yield_100/part02.json','./data/high_yield_100/part03.json','./data/high_yield_100/part04.json'];
+const CACHE='neetpg2027-v2-2026-09-15-timer1';
+const ASSETS=['./','./index.html','./assets/app.css','./assets/app.js','./assets/neetpg-timer.js','./manifest.webmanifest','./data/high_yield_100/part01.json','./data/high_yield_100/part02.json','./data/high_yield_100/part03.json','./data/high_yield_100/part04.json'];
 async function prime(){const c=await caches.open(CACHE);await c.addAll(ASSETS)}
 self.addEventListener('install',e=>{e.waitUntil(prime().then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('neetpg2027-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
