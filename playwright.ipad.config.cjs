904a219fe163612c2c40320a9b54d70b3a2337e0
Md Moazzam <mdmoazzam291@gmail.com>
2026-09-15 17:53:04 +0530

    Add WebKit iPad test configuration

---
+++ playwright.ipad.config.cjs
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: 'tests',
+  testMatch: ['phase13_ipad_webkit.spec.js'],
+  use: {
+    baseURL: 'http://127.0.0.1:4173',
+    browserName: 'webkit',
+    viewport: { width: 834, height: 1194 },
+    hasTouch: true
+  },
+  reporter: 'line',
+  timeout: 30000
+});
