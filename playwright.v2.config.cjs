b08e9d3aedb695a1c6b588318f1bfe9382096d76
Md Moazzam <mdmoazzam291@gmail.com>
2026-09-15 06:49:02 +0530

    Add isolated browser test config for v2

---
+++ playwright.v2.config.cjs
@@ -0,0 +1,8 @@
+const { defineConfig } = require('@playwright/test');
+module.exports=defineConfig({
+  testDir:'tests',
+  testMatch:'github_only_v2.spec.js',
+  use:{baseURL:'http://127.0.0.1:4173',browserName:'chromium'},
+  reporter:'line',
+  timeout:30000
+});
